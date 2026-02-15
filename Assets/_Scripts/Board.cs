731d6d50304b85bd7c4afe97152f7a822efdd90c
Phạm Tiến Thắng <132671977+PhamTienThang2510@users.noreply.github.com>
2026-02-16 00:36:34 +0700

    Move swap logic to Board and add DOTween
    Refactor match-3 swapping: centralize swap animation and array/coordinate updates in Board (TrySwap, SwapWithAnimation coroutine) using DOTween, and add a CanMove flag to block input during animations. Simplify board setup (instantiate tiles/dots, set Dot.column/row/board, rename Dots->dots) and position the camera. Update Dot to only detect swipes and compute target cell (higher swipe threshold) then call board.TrySwap; remove per-dot tweening/state and unused imports. Overall cleans up responsibilities and ensures consistent swapping behavior.

--- Assets/_Scripts/Board.cs
+++ Assets/_Scripts/Board.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
+using DG.Tweening;
+using System.Collections;
 
 public class Board : MonoBehaviour
 {
-
+    [Header("Board Settings")]
     [SerializeField] private int width = 8;
     [SerializeField] private int height = 8;
     [SerializeField] private GameObject tilePrefab;
-    [SerializeField] private GameObject[] Dots;
-    private TileBackground[,] gems;
+    [SerializeField] private GameObject[] dots;
+
     public int Width => width;
     public int Height => height;
+
+    public bool CanMove { get; private set; } = true;
+
     public GameObject[,] AllDotsInTheMatch;
 
     private void Start()
     {
-        gems = new TileBackground[width, height];
         AllDotsInTheMatch = new GameObject[width, height];
         SetUpBoard();
         SetUpCamera();
     }
+
+    #region Setup
 
     private void SetUpBoard()
     {
@@ -26,26 +32,82 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                Vector2 PosTemp = new Vector2(x, y);
-                GameObject TileBackground = Instantiate(tilePrefab, PosTemp, Quaternion.identity, transform) as GameObject;
-                TileBackground.transform.parent = this.transform;
-                TileBackground.name = $"({x}, {y})";
-                int randomDot = Random.Range(0, Dots.Length);
-                GameObject dot = Instantiate(Dots[randomDot], PosTemp, Quaternion.identity, transform) as GameObject;
-                dot.transform.parent = this.transform;
-                dot.name = $"({x}, {y})";
-                AllDotsInTheMatch[x, y] = dot;
+                Vector2 pos = new Vector2(x, y);
+
+                Instantiate(tilePrefab, pos, Quaternion.identity, transform);
+
+                int randomDot = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[randomDot], pos, Quaternion.identity, transform);
+
                 Dot dotScript = dot.GetComponent<Dot>();
                 dotScript.column = x;
                 dotScript.row = y;
                 dotScript.board = this;
 
+                AllDotsInTheMatch[x, y] = dot;
             }
         }
     }
 
     private void SetUpCamera()
     {
-        Camera.main.transform.position = new Vector3(width / 2f - 0.5f, height / 2f - 0.5f, -10);
+        Camera.main.transform.position =
+            new Vector3(width / 2f - 0.5f, height / 2f - 0.5f, -10);
     }
+
+    #endregion
+
+    #region Swap
+
+    public void TrySwap(int col1, int row1, int col2, int row2)
+    {
+        if (!IsInsideBoard(col2, row2))
+            return;
+
+        StartCoroutine(SwapWithAnimation(col1, row1, col2, row2));
+    }
+
+    private bool IsInsideBoard(int col, int row)
+    {
+        return col >= 0 && col < width &&
+               row >= 0 && row < height;
+    }
+
+    private IEnumerator SwapWithAnimation(int col1, int row1, int col2, int row2)
+    {
+        CanMove = false;
+
+        GameObject dot1 = AllDotsInTheMatch[col1, row1];
+        GameObject dot2 = AllDotsInTheMatch[col2, row2];
+
+        Vector2 pos1 = dot1.transform.position;
+        Vector2 pos2 = dot2.transform.position;
+
+        float duration = 0.25f;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Join(dot1.transform.DOMove(pos2, duration).SetEase(Ease.OutQuad));
+        seq.Join(dot2.transform.DOMove(pos1, duration).SetEase(Ease.OutQuad));
+
+        yield return seq.WaitForCompletion();
+
+        // Swap in array
+        AllDotsInTheMatch[col1, row1] = dot2;
+        AllDotsInTheMatch[col2, row2] = dot1;
+
+        // Update logical coordinates
+        Dot dot1Script = dot1.GetComponent<Dot>();
+        Dot dot2Script = dot2.GetComponent<Dot>();
+
+        dot1Script.column = col2;
+        dot1Script.row = row2;
+
+        dot2Script.column = col1;
+        dot2Script.row = row1;
+
+        CanMove = true;
+    }
+
+    #endregion
 }
--- Assets/_Scripts/Dot.cs
+++ Assets/_Scripts/Dot.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using DG.Tweening;
-using System.Collections;
 
 public class Dot : MonoBehaviour
 {
@@ -8,109 +6,45 @@ public class Dot : MonoBehaviour
     [HideInInspector] public int row;
     [HideInInspector] public Board board;
 
-    private GameObject target;
-
     private Vector2 firstTouch;
     private Vector2 lastTouch;
-    private float swipeAngle;
-
-    private bool isSwapping = false;
 
     private void OnMouseDown()
     {
-        if (isSwapping) return;
+        if (!board.CanMove) return;
 
         firstTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void OnMouseUp()
     {
-        if (isSwapping) return;
+        if (!board.CanMove) return;
 
         lastTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        CalculateAngle();
+        CalculateSwipe();
     }
 
-    void CalculateAngle()
+    private void CalculateSwipe()
     {
-        if (Vector2.Distance(firstTouch, lastTouch) < 0.2f)
+        if (Vector2.Distance(firstTouch, lastTouch) < 0.3f)
             return;
 
-        swipeAngle = Mathf.Atan2(
+        float angle = Mathf.Atan2(
             lastTouch.y - firstTouch.y,
             lastTouch.x - firstTouch.x) * Mathf.Rad2Deg;
 
-        SwipePieces();
-    }
+        int targetColumn = column;
+        int targetRow = row;
 
-    void SwipePieces()
-    {
-        target = null;
+        if (angle > -45 && angle <= 45)          // RIGHT
+            targetColumn++;
+        else if (angle > 45 && angle <= 135)     // UP
+            targetRow++;
+        else if (angle > -135 && angle <= -45)   // DOWN
+            targetRow--;
+        else                                     // LEFT
+            targetColumn--;
 
-        // RIGHT
-        if (swipeAngle > -45 && swipeAngle <= 45)
-        {
-            if (column < board.Width - 1)
-                target = board.AllDotsInTheMatch[column + 1, row];
-        }
-        // UP
-        else if (swipeAngle > 45 && swipeAngle <= 135)
-        {
-            if (row < board.Height - 1)
-                target = board.AllDotsInTheMatch[column, row + 1];
-        }
-        // DOWN
-        else if (swipeAngle > -135 && swipeAngle <= -45)
-        {
-            if (row > 0)
-                target = board.AllDotsInTheMatch[column, row - 1];
-        }
-        // LEFT
-        else
-        {
-            if (column > 0)
-                target = board.AllDotsInTheMatch[column - 1, row];
-        }
-
-        if (target != null)
-        {
-            StartCoroutine(SwapWithAnimation());
-        }
-    }
-
-    IEnumerator SwapWithAnimation()
-    {
-        isSwapping = true;
-
-        Dot targetDot = target.GetComponent<Dot>();
-
-        Vector2 startPos = transform.position;
-        Vector2 targetPos = target.transform.position;
-
-        float duration = 0.25f;
-
-        Sequence seq = DOTween.Sequence();
-
-        seq.Join(transform.DOMove(targetPos, duration).SetEase(Ease.OutQuad));
-        seq.Join(target.transform.DOMove(startPos, duration).SetEase(Ease.OutQuad));
-
-        yield return seq.WaitForCompletion();
-
-        // --- Update board array ---
-        board.AllDotsInTheMatch[column, row] = target;
-        board.AllDotsInTheMatch[targetDot.column, targetDot.row] = gameObject;
-
-        // --- Swap logic coordinates ---
-        int tempColumn = targetDot.column;
-        int tempRow = targetDot.row;
-
-        targetDot.column = column;
-        targetDot.row = row;
-
-        column = tempColumn;
-        row = tempRow;
-
-        target = null;
-        isSwapping = false;
+        board.TrySwap(column, row, targetColumn, targetRow);
     }
 }
